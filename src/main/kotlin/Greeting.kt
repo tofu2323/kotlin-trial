ef57fe2520a1ab5e0f5797c4959a79fa8360f919
Fumiya Goto <gotch@styz.io>
2017-12-10 16:20:08 +0900

    first commit

---
+++ src/main/kotlin/Greeting.kt
@@ -0,0 +1,6 @@
+package main.kotlin
+
+data class Greeting(
+        val id: Long,
+        val content: String
+)
---
+++ src/main/kotlin/GreetingController.kt
@@ -0,0 +1,15 @@
+package main.kotlin
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+    var counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+}
